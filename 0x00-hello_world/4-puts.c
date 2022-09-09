33ca5f840152de51587efffd7a2f2fb5a77d1850
oyioguv <oyioguv@gmail.com>
2022-09-08 18:37:00 -0700

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - entry point
+ * main - Entry point
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
